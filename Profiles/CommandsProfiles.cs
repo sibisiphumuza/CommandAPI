d3405d6c6a7080781388e119fe4afb0171d0aa50
sibisiphumuza <sibisi.ps@gmail.com>
2022-10-24 16:45:20 +0200

    Initial Commit

---
+++ Profiles/CommandsProfiles.cs
@@ -0,0 +1,18 @@
+using AutoMapper;
+using SixMinApi.Dtos;
+using SixMinApi.Models;
+
+namespace SixMinApi.Profiles
+{
+
+    public class CommandProfile : Profile
+    {
+        public CommandProfile()
+        {
+            // Source -> Target
+            CreateMap<Command, CommandReadDto>();
+            CreateMap<CommandCreateDto, Command>();
+            CreateMap<CommandUpdateDto, Command>();
+        }
+    }
+}
